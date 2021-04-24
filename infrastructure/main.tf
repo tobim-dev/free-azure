1d7b3ddac3292c166e3cd5bd5e13a7108a356332
tobim-dev <mail@tmeyer.me>
2021-04-24 21:45:45 +0200

    add function app to tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,7 +14,79 @@ provider "azurerm" {
   features {}
 }
 
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "free-azure"
   location = "westeurope"
+}
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "tfex-cosmos-db-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+  enable_free_tier    = true
+
+  enable_automatic_failover = false
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+}
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_storage_account" "storageAccount" {
+  name                     = "functionsapp${random_integer.ri.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "servicePlan" {
+  name                = "service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Consumption"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "functionApp" {
+  name                       = "function-app-${random_integer.ri.result}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.servicePlan.id
+  storage_account_name       = azurerm_storage_account.storageAccount.name
+  storage_account_access_key = azurerm_storage_account.storageAccount.primary_access_key
 }
